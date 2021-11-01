7cad0eee04970f742d4f7c590ecd717d8da26895
Ashenafi-Tesfaye <rashenafibab@gmail.com>
2021-11-01 11:18:23 -0400

    initial commit

---
+++ bookstore-withSpring/src/main/java/com/ashenafiAG/spring/bookstore/bookstorewithSpring/BookstoreWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ashenafiAG.spring.bookstore.bookstorewithSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreWithSpringApplication.class, args);
+	}
+
+}
---
+++ bookstore-withSpring/src/test/java/com/ashenafiAG/spring/bookstore/bookstorewithSpring/BookstoreWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashenafiAG.spring.bookstore.bookstorewithSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
